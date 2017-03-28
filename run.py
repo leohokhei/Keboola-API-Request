e927ffe561075d3e9f710ecb04de90e839297199
leohokhei <chanleoc@alumni.sfu.ca>
2017-03-27 21:45:41 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 ###########################################################################################
+### Keboola API Request                                                                 ###
 ### Prepared by Leo Chan                                                                ###
 ### Python Ver 2.7                                                                      ###
+### Requirements:                                                                       ###
+### API request and output into CSV                                                     ###
+### Details:                                                                            ###
 ### 1. This program will output everything but the ones which are not completed.        ###
 ### 2. This program can output up to as many forms it is available with the given API.  ###
 ### 3. NaN are used for responses that are missing.                                     ###
